3e1dbcae654cd134474ae4d5d6f3c346ec56ed95
Virrum <virrum@gmail.com>
2016-12-14 13:36:37 +0100

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,7 @@
     'use strict';
     var socketUrl = 'ws://' + location.host;
     var WebSocket = new WebSocket(socketUrl);
-    console.log(socketUrl);
+    console.log(socketUrl, WebSocket);
     var app = {
         // webSocket = new WebSocket(socketUrl),
         heading: document.querySelector("h1"),
